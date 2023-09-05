c443b64c3dddb10f59da024ab21d26791529829b
kim <joakimbwire23@gmail.com>
2023-09-05 03:02:04 +0300

    Ensured the parser is working properly

--- dp0426/parsers.js
+++ dp0426/parsers.js
@@ -1,27 +1,44 @@
 function parsePage({responseBody, URL, html, referer}) {
     console.log(`parsePage: parsing: ${responseBody.fileFormat} ${URL}`);
     let json;
-    try {
-        json = JSON.parse(responseBody.content);
-    } catch (e) {
-        console.error("error parsing json ", e);
-        return [];
-    }
+    let results = []
+    json = JSON.parse(responseBody.content);
+ 
+
     let doc = {
+        URI:[], 
         numeroDeCaso:null,
+        numero:null,
         tipoDeAccion:null,
         processoDeOrigen:null,
         motivo:null,
-        desicionResumen:null,
-        desicion:null,
+        decisionResumen:null,
+        decision:null,
         conceptos:null,
         derechos:null,
         legitimados:null,
         casosAcumulados:null,
-        senteciasRelacionados:null
-
+        senteciasRelacionados:null,
+        URL:[URL]
     }
-    return [doc];
+    
+    doc.URI = [json.dato.documento.uuid];
+    doc.documentType = json.dato.documento.nombreDocumento;
+    doc.fechaDecision = json.dato.fechaDecision;
+    doc.numeroDeCaso = json.dato.causa.numero
+    doc.numero = json.numero
+    doc.tipoDeAccion = json.dato.causa.tipoAccion.nombre
+    doc.processoDeOrigen = json.procesoOrigen
+    doc.motivo = json.dato.motivo
+    doc.decision = json.dato.decision.nombre
+    doc.decisionResumen = json.dato.decision.resumenDecision
+    doc.conceptos = json.dato.conceptos
+    let legitimados = json.dato.legitimados;
+    results.push(doc)
+        
+    
+
+    return results;
 }
 
 
